ef8023a2e9e118c9144eada61c23559e69471f65
Sagun Karki <sagunk064@gmail.com>
2023-02-04 13:16:13 -0600

    added models and routes

---
+++ server/routes/ideaPosts.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import { getIdeas, getIdeasBySearch, getIdeasByAuthor, getIdea, createIdea, likeIdea, commentIdea } from '../controllers/posts.js';
+
+const router = express.Router();
+import auth from "../middleware/auth.js";
+
+router.get('/author', getIdeasByAuthor);
+router.get('/search', getIdeasBySearch);
+router.get('/', getIdeas);
+router.get('/:id', getIdea);
+
+router.post('/', auth,  createIdea);
+router.patch('/:id/likeIdea', auth, likeIdea);
+router.post('/:id/commentIdea', commentIdea);
+
+export default router;
