2d635721f4f71403f88e4eb3206db591ea1d02b7
Kashish-Syed <ksyed2@huskers.unl.edu>
2023-02-04 15:14:22 -0600

    database connection

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import ideaRoutes from './routes/ideaPosts.js';
 
 const app = express();
 
@@ -11,8 +11,7 @@ app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes);
-app.use("/user", userRouter);
+app.use('/', ideaRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://dev-ritvik:ABCD1234@cluster0.qviluhd.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5100;
--- server/routes/ideaPosts.js
+++ server/routes/ideaPosts.js
@@ -1,17 +1,18 @@
 import express from 'express';
 
-import { getIdeas, getIdeasBySearch, getIdeasByAuthor, getIdea, createIdea, likeIdea, commentIdea } from '../controllers/posts.js';
+import { getIdeas, getIdeasBySearch, getIdea, createIdea, likeIdea, commentIdea, getIdeasByCreator } from '../controllers/ideas.js';
 
 const router = express.Router();
-import auth from "../middleware/auth.js";
+// import auth from "../middleware/auth.js";
 
-router.get('/author', getIdeasByAuthor);
+router.get('/author', getIdeasByCreator);
 router.get('/search', getIdeasBySearch);
 router.get('/', getIdeas);
 router.get('/:id', getIdea);
 
-router.post('/', auth,  createIdea);
-router.patch('/:id/likeIdea', auth, likeIdea);
+router.post('/createIdea', createIdea);
+
+router.patch('/:id/likeIdea', likeIdea);
 router.post('/:id/commentIdea', commentIdea);
 
 export default router;
